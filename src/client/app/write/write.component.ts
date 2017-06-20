8092906e031dbf2f71bc524022eecee8145d64f1
Jason Zhou <zhou.jasony@gmail.com>
2017-06-19 23:42:42 -0700

    touch up autosave and displays

--- src/client/app/write/write.component.ts
+++ src/client/app/write/write.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { QuestionContainerComponent } from './question-container.component';
 import { WeeksService } from '../shared/weeks/weeks.service';
 
-import moment from 'moment';
+import * as moment from 'moment';
 
 @Component({
   moduleId: module.id,
@@ -12,16 +12,33 @@ import moment from 'moment';
 })
 export class WriteComponent implements OnInit {
 
-  week: object;
+  week: any;
+  selectedDay: {
+    date: string,
+    dayOfWeek: number
+  } = {
+    date: '',
+    dayOfWeek: -1
+  };
 
   constructor(public weeksService: WeeksService) {}
 
   ngOnInit() {
-    this.populate();
+    let this_week = moment().startOf('isoWeek').format('YYYY-MM-DD');
+    this.selectedDay.date = moment().format('dddd, MMMM Do');
+    this.selectedDay.dayOfWeek = moment().day();
+    this.populate(this_week);
   }
 
-  populate() {
-    this.weeksService.get('2017-06-19')
+  getDisplayName(week_string: string): string {
+    let endMonth = moment(week_string).endOf('isoWeek').month();
+    let suffix = endMonth == moment(week_string).month() ? '' : endMonth;
+    let prefix = moment().startOf('isoWeek').format('YYYY-MM-DD') == week_string ? 'This Week: ' : '';
+    return `${prefix}${moment(week_string).format('MMMM Do')} - ${suffix} ${moment(week_string).endOf('isoWeek').format('Do')}`;
+  }
+
+  populate(week_string: string) {
+    this.weeksService.get(week_string)
       .subscribe(
         week => this.week = week
       );
